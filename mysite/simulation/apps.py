488a61f2dfdfc0ac9756c512b2128bf548f3b520
Sagar Saby <sabysagar@gmail.com>
2023-10-14 16:45:35 -0700

    Display simulation app at /simulation

--- mysite/simulation/apps.py
+++ mysite/simulation/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class VitalityConfig(AppConfig):
+class SimulationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vitality'
+    name = 'simulation'
--- mysite/simulation/views.py
+++ mysite/simulation/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def ingredients(ingredientString: str):
     pass
 
 # Create your views here.
 def simulation(request):
-    return HttpResponse("hello")
+    template = loader.get_template("simulation.html")
+    return HttpResponse(template.render())
