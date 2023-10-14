91b54a793bf56625bba07f648a88712b3cd75772
Rcontributer <147943392+RaghavR125@users.noreply.github.com>
2023-10-14 15:28:22 -0700

    urls added

--- mysite/simulation/urls.py
+++ mysite/simulation/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('simulation', views.simulation, name = 'Run')]
+urlpatterns = [path('simulation/', views.simulation, name = 'simulation')]
